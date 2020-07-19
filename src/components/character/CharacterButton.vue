9d330ce98342033f1ed01544b12e08f912d65721
nitoa <kitamakeai2025@gmail.com>
2020-07-20 00:45:14 +0900

    キャラのコンポーネント追加

---
+++ src/components/character/CharacterButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class = 'character__button' :class = "{ active: isActive }" @click = 'onclick'>
+    <img class = 'character__image' :src = "require(`@/assets/characters/${characterFileName}`)" />
+  </button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isActive: false
+    }
+  },
+  props: [ 'characterFileName' ],
+  methods: {
+    onclick () {
+      this.isActive = true;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.character__button {
+  width: 100px;
+  height: 100px;
+  border: 0;
+  padding: 0;
+}
+
+.character__image {
+  width: 100%;
+  height: 100%;
+}
+
+.active {
+  border: solid 1px red;
+}
+.active:focus {
+  outline: none;
+}
+</style>
