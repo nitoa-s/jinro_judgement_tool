b5dfa54fbb49570bea72ca67fbc0655d5a89d273
nisutoa_windows <kitamakeai2025@gmail.com>
2020-07-30 00:10:57 +0900

    role情報追加

---
+++ src/components/modules/RoleButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class = 'role__image' :src = "require(`@/assets/roles/${roleName}.png`)" />
+</template>
+
+<script>
+export default {
+  props: ['roleName']
+}
+</script>
+
+<style scoped>
+
+.role__image {
+  width: 100px;
+  height: auto;
+  object-fit: contain;
+}
+</style>
---
+++ src/components/pages/AddRoles.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class = 'role__area'>
+		<roleButton
+			v-for = 'role in roleData'
+			:key = 'role.id'
+			:roleName = 'role.name'></roleButton>
+	</div>
+</template>
+
+<script>
+import roles from '@/assets/roleData.json'
+import RoleButton from '@/components/modules/RoleButton'
+export default {
+	data () {
+		return {
+			roleData : roles
+		}
+	},
+	components: {
+		'roleButton': RoleButton
+	}
+}
+</script>
+
+<style scoped>
+.role__area {
+	width: 500px;
+}
+</style>
