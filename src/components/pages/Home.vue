04a9f4790a38e1ea3a3ba8e48f911720beef6a97
nisutoa_windows <kitamakeai2025@gmail.com>
2020-08-01 00:07:08 +0900

    キャラ画像表示

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,23 +1,41 @@
 <template>
   <div class = 'top_area'>
-    <div class = 'join-Character_area'>
-      <div v-for = 'character in joinCharacter' :key = 'character'>
-        <img class = 'character_image' :src = "require(`@/assets/characters/${character}.png`)" @click.left = 'check'/>
+    <div class = 'join_member_area'>
+      <div class = 'member_row_area' v-for = '(characterRow, index) in groupCharacters' :key = 'index'>
+        <img class = 'member_image' v-for = '(character, index) in characterRow' :key = 'index' :src = 'imagePath(character.name, character.imageIndex)'/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import characterData from '@/assets/characterData.json'
 export default {
   data () {
     return {
-      joinCharacter: JSON.parse(localStorage.getItem('jinroMembers'))
+      characterData,
+      characters: this.$store.getters.jinroMembers,
+      sliceNum: 5
+    }
+  },
+  computed: {
+    groupCharacters () {
+      const arrayLength = this.characters.length
+      const groupedArray = []
+      for( let index = 0; index < Math.ceil(arrayLength / this.sliceNum); index++ ) {
+        let groupedFirstIndex = index * this.sliceNum;
+        groupedArray.push(this.characters.slice(groupedFirstIndex, groupedFirstIndex + this.sliceNum))
+      }
+      return groupedArray
     }
   },
   methods: {
-    check () {
-      console.log('テスト')
+    imagePath (memberName, imageIndex) {
+      let imageName = null
+      for( let index = 0; index < this.characterData.length; index++)
+        if( this.characterData[index].name === memberName )
+          imageName = this.characterData[index].image[imageIndex]
+      return require(`@/assets/characters/${memberName}/${imageName}`)
     }
   }
 }
@@ -28,12 +46,21 @@ export default {
   padding: 10px;
 }
 
-.join-Character_area {
-  width: 500px;
-  display: flex;
+.join_member_area {
+  padding: 0 5px;
 }
-.character_image {
-  width: 100px;
-  height: auto;
+
+.member_row_area {
+  display: flex;
+  margin: 5px 0;
+  height: 80px;
+  flex-direction: row;
+}
+
+.member_image {
+  margin: 1px;
+  width: 80px;
+  height: 80px;
+  user-select: none;
 }
 </style>
