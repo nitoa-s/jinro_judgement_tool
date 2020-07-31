cfa6fb2a6be1ed0236880d8f460c0377656d305a
nisutoa_windows <kitamakeai2025@gmail.com>
2020-07-31 17:34:24 +0900

    最低限完成

--- src/components/modules/RoleButton.vue
+++ src/components/modules/RoleButton.vue
@@ -1,12 +1,40 @@
 <template>
-  <button class = 'role_button'>
+  <button class = 'role_button' @click.left = 'join' @click.right = 'remove'>
       <img class = 'role__image' :src = "require(`@/assets/roles/${camp}/${roleData.name}.png`)" />
   </button>
 </template>
 
 <script>
 export default {
-  props: ['roleData', 'camp']
+  props: [
+    'roleData',
+    'camp'
+  ],
+  methods: {
+    join() {
+      var roles = JSON.parse(localStorage.getItem('roles'));
+      if( roles === null ) {
+        roles = [];
+        roles.push(this.roleData);
+        localStorage.setItem('roles', JSON.stringify(roles));
+      } else {
+        for( var index = 0; index < roles.length ; index ++ )
+          if( roles[index].name === this.roleData.name ) return;
+        roles.push(this.roleData);
+        localStorage.setItem('roles', JSON.stringify(roles));
+      }
+    },
+    remove() {
+      var roles = JSON.parse(localStorage.getItem('roles'));
+      if( roles !== null) {
+        for( var index = 0; index < roles.length ; index ++ )
+          if( roles[index].name === this.roleData.name ) {
+            roles.splice(index, 1);
+            localStorage.setItem('roles', JSON.stringify(roles));
+          }
+      }
+    }
+  }
 }
 </script>
 
